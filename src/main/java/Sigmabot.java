20f9c1ad5978432cf746cbeabb4a622590eb1588
roman.yanushevskyi <wailydest@gmail.com>
2025-01-30 16:40:00 +0800

    add mark as done feature

--- src/main/java/Sigmabot.java
+++ src/main/java/Sigmabot.java
@@ -1,12 +1,36 @@
 import java.util.ArrayList;
+import java.util.Optional;
 import java.util.Scanner;
 public class Sigmabot {
     static ArrayList<Task> taskList;
 
     public static void viewTaskList() {
         for (int i = 0; i < taskList.size(); ++i) {
-            System.out.println("Task " + (i + 1) + ": " + taskList.get(i).toString());
+            System.out.println("["
+                    + (taskList.get(i).getIsMarked() ? "X" : " " )
+                    + "] Task " + (i + 1) + ": "
+                    + taskList.get(i).toString());
         }
+    }
+    public static boolean processMarkInput(String input) {
+        String[] inputParts = input.split("\\s+");
+        if (inputParts.length == 2
+                && (inputParts[0].equals("mark") || inputParts[0].equals("unmark"))) {
+            try {
+                int taskNumber = Integer.parseInt(inputParts[1]);
+                if (taskNumber < 1 || taskNumber > Sigmabot.taskList.size()) {
+                    throw new IllegalArgumentException("Task " + taskNumber + " does not exist");
+                }
+                --taskNumber;
+                if (inputParts[0].equals("mark")) taskList.get(taskNumber).mark();
+                else taskList.get(taskNumber).unmark();
+                return true;
+            } catch (NumberFormatException e) {
+                System.out.println("invalid " + inputParts[0] + " request: " + e);
+                throw e;
+            }
+        }
+        return false;
     }
     public static void main(String[] args) {
         Sigmabot.taskList = new ArrayList<Task>();
@@ -15,10 +39,10 @@ public class Sigmabot {
 
         Scanner scanner = new Scanner(System.in);
         while (true) {
-            String input = scanner.nextLine();
+            String input = scanner.nextLine().trim();
             if (input.equals("bye")) break;
             if (input.equals("list")) Sigmabot.viewTaskList();
-            else taskList.add(new Task(input));
+            else if (!Sigmabot.processMarkInput(input)) taskList.add(new Task(input));
         }
 
         System.out.println("see you soon!");
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,7 +1,18 @@
 public class Task {
-    String description;
+    private String description;
+    private boolean isMarked;
     public Task(String description) {
         this.description = description;
+        this.isMarked = false;
+    }
+    public void mark() {
+        this.isMarked = true;
+    }
+    public void unmark() {
+        this.isMarked = false;
+    }
+    public boolean getIsMarked() {
+        return isMarked;
     }
     @Override
     public String toString() {
