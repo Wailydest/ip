b4928ccf4569624c5a4b6b8b0052915a68ed1ad9
roman.yanushevskyi <wailydest@gmail.com>
2025-01-30 14:19:01 +0800

    add echo feature

--- src/main/java/Sigmabot.java
+++ src/main/java/Sigmabot.java
@@ -1,6 +1,14 @@
+import java.util.Scanner;
+
 public class Sigmabot {
     public static void main(String[] args) {
         System.out.println("hi! i'm your sigma bot");
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("bye")) break;
+            System.out.println(input);
+        }
         System.out.println("see you soon!");
     }
 }
