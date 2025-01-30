a0ae1092062013e3c10e804cde3e444c62ddd59b
roman.yanushevskyi <wailydest@gmail.com>
2025-01-30 16:07:10 +0800

    add list

--- src/main/java/Sigmabot.java
+++ src/main/java/Sigmabot.java
@@ -1,14 +1,26 @@
+import java.util.ArrayList;
 import java.util.Scanner;
-
 public class Sigmabot {
+    static ArrayList<Task> taskList;
+
+    public static void viewTaskList() {
+        for (int i = 0; i < taskList.size(); ++i) {
+            System.out.println("Task " + (i + 1) + ": " + taskList.get(i).toString());
+        }
+    }
     public static void main(String[] args) {
-        System.out.println("hi! i'm your sigma bot");
+        Sigmabot.taskList = new ArrayList<Task>();
+
+        System.out.println("hi! i'm your sigma bot. what's on your list?");
+
         Scanner scanner = new Scanner(System.in);
         while (true) {
             String input = scanner.nextLine();
             if (input.equals("bye")) break;
-            System.out.println(input);
+            if (input.equals("list")) Sigmabot.viewTaskList();
+            else taskList.add(new Task(input));
         }
+
         System.out.println("see you soon!");
     }
 }
---
+++ src/main/java/Task.java
@@ -0,0 +1,10 @@
+public class Task {
+    String description;
+    public Task(String description) {
+        this.description = description;
+    }
+    @Override
+    public String toString() {
+        return description;
+    }
+}
