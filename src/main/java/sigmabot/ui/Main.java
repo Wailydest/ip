b28336c197059109a6341cb1c481d25a14a7c040
roman.yanushevskyi <wailydest@gmail.com>
2025-02-21 18:27:44 +0800

    display application name

--- src/main/java/sigmabot/ui/Main.java
+++ src/main/java/sigmabot/ui/Main.java
@@ -18,6 +18,7 @@ public class Main extends Application {
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
             stage.setScene(scene);
+            stage.setTitle("SigmaBot");
             fxmlLoader.<MainWindow>getController().setSigmabot(sigmabot);
             stage.show();
         } catch (IOException e) {
